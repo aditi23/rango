1f7e2e4ed8fd8a5d9bc640665eb5ce56cdab30ea
aditi23 <aditisinghsikarwar@gmail.com>
2016-11-30 00:27:22 +0530

    test cases

--- tango/tests.py
+++ tango/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from tango.models import Category
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class CategoryMethodTest(TestCase):
+
+    def test_ensure_views_are_positive(self):
+        cat = Category(name='test',views=-1,likes=0)
+        cat.save()
+        self.assertEqual((cat.views>=0),True)
+
+    def test_slug_line_creation(self):
+        cat = cat('Random Category String')
+        cat.save()
+        self.assertEqual(cat.slug,'random-category-string')
+
+
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
