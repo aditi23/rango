b24897b0f567e5c199690a90e6a1ce1e5d6070d4
aditi23 <aditisinghsikarwar@gmail.com>
2016-12-01 15:57:59 +0530

    user registration added

--- tango/urls.py
+++ tango/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # url(r'^$',views.index,name='index'),
     url(r'^$',views.IndexView.as_view(),name='index'),
     url(r'^about/',views.about,name='about'),
-    url(r'^category/(?P<slug>[\w-]+)/$',views.ShowCategoryView.as_view(),name='show_category')
+    url(r'^category/(?P<slug>[\w-]+)/$',views.ShowCategoryView.as_view(),name='show_category'),
+    url(r'^register/',views.UserRegisterView.as_view(),name='register'),
 ]
--- tango/views.py
+++ tango/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from tango.models import Page, Category
+from tango.forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from django.views.generic import FormView
+from django.views.generic import View
 
 
 # def index(request):
@@ -89,3 +92,37 @@ def about(request):
     context_dict = {'aboutmessage': 'This tutorial has been put together by Aditi'}
     return render(request, 'tango/about.html', context=context_dict)
     # return HttpResponse("this is about page <br> <a href = '/tango'>Home</a>")
+
+
+class UserRegisterView(View):
+    form_class = UserRegisterForm
+    template_name = 'tango/register.html'
+
+    def get(self,request):
+        form = self.form_class(None)
+        return render(request,self.template_name,{'form':form})
+
+    def post(self,request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request,user)
+                    return redirect('/tango')
+
+        return render(request, self.template_name, {'form': form})
+
+
+
